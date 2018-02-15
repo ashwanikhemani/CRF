c58a823576af5bb06ceeada264996e93af14fb3a
G M <gmarat19@gmail.com>
2018-02-14 22:13:46 -0600

    add comments to clarify the list structures

--- code/read_data.py
+++ code/read_data.py
@@ -1,4 +1,6 @@
 #script to read input data into list structure
+#dataX number of examples by 128 "array"
+#dataY number of examples by 2 (each example has a label and a qid)
 
 with open("../data/train_struct.txt", "r") as f:
 	raw_data = f.read()
@@ -7,7 +9,7 @@ raw_data = raw_data.split("\n")
 dataX, dataY = [], []
 for line in raw_data[:-2]: #-2 because last element is empty
 	line = line.split(" ")
-	dataY.append([ int(line[0]), int(line[1][4:]) ]) 
+	dataY.append([ int(line[0]), int(line[1][4:]) ])
 	datax = [0]*128
 	for f1 in line[2:]:
 		end = f1.find(":")
