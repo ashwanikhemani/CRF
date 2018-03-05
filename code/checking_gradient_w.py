efcde5d0e98dfa2c391187068f3c739aa3532cc7
G M <gmarat19@gmail.com>
2018-03-04 19:02:52 -0600

    add promising gradient computating script

--- code/checking_gradient_w.py
+++ code/checking_gradient_w.py
@@ -70,84 +70,73 @@ def log_p_w(W, X, y, T):
 
 	return sum_num - log_z
 
-def fb_prob(X, y_i, y_i_pos, W, T):
-#computes the marginal prob of y_i by incorporating backward messages
+def fb_prob(X, W, T):
+#returns forward trellis and backward trellis and normalizer
 #y_i is the label and y_i_pos is the letter position
 	alpha_len = 26
-	trellis = numpy.zeros((X.shape[0], alpha_len))
+	trellisfw = numpy.zeros((X.shape[0], alpha_len))
+	trellisbw = numpy.zeros((X.shape[0], alpha_len))
 	interior = numpy.zeros(alpha_len)
 
 	#forward part
-	for i in range(1, y_i_pos):
+	for i in range(1, X.shape[0]):
 		for j in range(alpha_len):
 			for k in range(alpha_len):
 				interior[k] = numpy.dot(W[k], X[i-1]) +\
-					T[k,j] + trellis[i-1, k]
+					T[k,j] + trellisfw[i-1, k]
 			M = numpy.max(interior)
 			numpy.add(interior, -1*M, out=interior)
 			numpy.exp(interior, out=interior)
-			trellis[i, j] = M + math.log(numpy.sum(interior))
+			trellisfw[i, j] = M + math.log(numpy.sum(interior))
 
-	forward_part = 0
-	if y_i_pos > 0:
-		for k in range(alpha_len):
-			interior[k] = numpy.dot(W[k], X[y_i_pos-1]) +\
-				T[k, y_i] + trellis[y_i_pos-1, k]
-		M = numpy.max(interior)
-		numpy.add(interior, -1*M, out=interior)
-		numpy.exp(interior, out=interior)
-		forward_part = M + math.log(numpy.sum(interior))
+	for i in range(alpha_len):
+		interior[k] = numpy.dot(W[k], X[-1]) + trellisfw[-1, k]
+	M = numpy.max(interior)
+	numpy.add(interior, -1*M, out=interior)
+	numpy.exp(interior, out=interior)
+	
+	log_z = M + math.log(numpy.sum(interior))
+
 
 	#backward part
-	for i in range(X.shape[0]-2, y_i_pos, -1):
+	for i in range(X.shape[0]-2, -1, -1):
 		for j in range(alpha_len):
 			for k in range(alpha_len):
 				interior[k] = numpy.dot(W[k], X[i+1]) +\
-					T[j, k] + trellis[i+1, k]
+					T[j, k] + trellisbw[i+1, k]
 			M = numpy.max(interior)
 			numpy.add(interior, -1*M, out=interior)
 			numpy.exp(interior, out=interior)
+			trellisbw[i, j] = M + math.log(numpy.sum(interior))
 	
 	
-	backward_part = 0
-	if y_i_pos < X.shape[0] - 1:
-		for k in range(alpha_len):
-			interior[k] = numpy.dot(W[k], X[y_i_pos+1]) +\
-				T[y_i, k] + trellis[y_i_pos+1, k]
-		M = numpy.max(interior)
-		numpy.add(interior, -1*M, out=interior)
-		numpy.exp(interior, out=interior)
-		backward_part = M + math.log(numpy.sum(interior))
-
-	#final result
-	prob = forward_part + backward_part + numpy.dot(W[y_i], X[y_i_pos])
-#	print(prob)
-	#i think that there needs to be some normalization here
-	return prob
+	return trellisfw, trellisbw, log_z
 
 def log_p_wgrad(W, X, y, T):
 #will compute the gradient of an example
 	grad = numpy.zeros((26, 128)) #size of the alphabet by 128 elems
 	expect = numpy.zeros(26)
 	letter_grad = numpy.zeros((26, 128))
-	prob = numpy.zeros(X.shape[0])
+	trellisfw, trellisbw, log_z = fb_prob(X, W, T)
 	for i in range(X.shape[0]):
-		prob[i] = fb_prob(X, y[i], i, W, T)
-	normalizer = numpy.sum(prob)
-	for i in range(X.shape[0]):
+		prob = (trellisfw[i, y[i]] + trellisbw[i, y[i]] +\
+			numpy.dot(W[y[i]], X[i]))/log_z
 		expect[y[i]] = 1
 		for j in range(26):
-			expect[j] -= prob[i]/normalizer
+			expect[j] -= prob/log_z
 			numpy.multiply(expect[j], X[i], out=letter_grad[j])
 		numpy.add(grad, letter_grad, out=grad)
 		expect[:] = 0
 	return grad
 
+grad = numpy.zeros((26, 128))
 import time
 
 t0 = time.time()
 for i in range(len(data)):
-	log_p_wgrad(W, data[i][0], data[i][1], T)
+	numpy.add(log_p_wgrad(W, data[i][0], data[i][1], T), grad, out=grad)
 t1 = time.time()
+
+print(f"{numpy.divide(grad, len(data))}")
 
 print(f"Time: {t1-t0}")
