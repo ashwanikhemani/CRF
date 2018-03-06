923a50dbe86ef8a59cb821bbbacd944246a63241
G M <gmarat19@gmail.com>
2018-03-05 20:06:32 -0600

    safety commit

--- code/optimize.py
+++ code/optimize.py
@@ -3,6 +3,7 @@ from scipy.optimize import fmin_bfgs
 from numpy.linalg import norm
 
 X_y = data_read.read_train()
+W, T = data_read.read_model()
 
 def func(params, *args):
 #objective function specified in the handout
@@ -34,6 +35,7 @@ def func_prime(params, *args):
 
 	#multiply C/N
 	numpy.multiply(log_grad_w, -1*C/len(data), out=log_grad_w)
+	numpy.multiply(log_grad_t, -1*C/len(data), out=log_grad_t)
 
 	#add gradient of norm
 	numpy.add(log_grad_w, W, out=log_grad_w)
@@ -41,10 +43,13 @@ def func_prime(params, *args):
 	#add normalizing factor
 	numpy.add(log_grad_t, T, out=log_grad_t)
 
-	return numpy.concatenate([log_grad_w.reshape(26*128), log_grad_t.reshape(26*26)])
+	return numpy.concatenate([log_grad_w.reshape(26*128),\
+		log_grad_t.reshape(26*26)])
 
+#on = numpy.concatenate([W.reshape(26*128), T.reshape(26*26)])
 
-#func([W, T], [X_y, 1000])
+#res = func(on, X_y[:9], 1000)
+#result = func_prime(on, X_y[:9], 1000)
 
 #need to flatten for the optimizer
 initial_guess = numpy.zeros((26*128+26*26))
@@ -52,5 +57,5 @@ initial_guess = numpy.zeros((26*128+26*26))
 #bounds = [(-10000000, 10000000)]*(28*128+26*26)
 
 
-x, f, d = fmin_bfgs(func, initial_guess, fprime=func_prime, args=(X_y, 1000))
-
+ret = fmin_bfgs(func, initial_guess, fprime=func_prime, args=(X_y[:5], 1000),\
+	maxiter=10, retall=True, full_output=True)
